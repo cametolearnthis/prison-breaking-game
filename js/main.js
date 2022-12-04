8922dcb47ed2e128e2fe1dd1e1daf4acfaa44333
Jorge Sanchez <jorgesinsilla@gmail.com>
2022-12-04 16:56:25 +0000

    create class light & position & domElement()

--- js/main.js
+++ js/main.js
@@ -52,8 +52,29 @@ class Prisoner {
   }
 }
 
-const prisoner = new Prisoner();
+class Light {
+  constructor() {
+    this.positionX = 50;
+    this.positionY = 80;
+    this.width = 5;
+    this.height = 20;
 
+    this.domElement = null;
+    this.createDomElement();
+  }
+  createDomElement() {
+    this.domElement = document.createElement("div");
+    this.domElement.className = "light";
+    this.domElement.style.width = this.width + "vw";
+    this.domElement.style.height = this.height + "vh";
+    this.domElement.style.left = this.positionX + "vw";
+    this.domElement.style.bottom = this.positionY + "vh";
+    const boardElm = document.getElementById("board");
+    boardElm.appendChild(this.domElement);
+  }
+}
+const prisoner = new Prisoner();
+const light = new Light();
 document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowUp") {
     prisoner.moveUp();
