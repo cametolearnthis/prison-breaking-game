6be01daadec14fab537914767eb87ea6e889b929
Jorge Sanchez <jorgesinsilla@gmail.com>
2022-12-05 11:46:13 +0000

    create hole & win.html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 class Prisoner {
   constructor() {
     this.positionX = 0;
-    this.positionY = 50;
-    this.width = 5;
-    this.height = 20;
+    this.positionY = 225;
+    this.width = 50;
+    this.height = 50;
 
     this.domElement = null;
     this.createDomElement();
@@ -11,20 +11,20 @@ class Prisoner {
   createDomElement() {
     this.domElement = document.createElement("div");
     this.domElement.id = "prisoner";
-    this.domElement.style.width = this.width + "vw";
-    this.domElement.style.height = this.height + "vh";
-    this.domElement.style.left = this.positionX + "vw";
-    this.domElement.style.bottom = this.positionY + "vh";
+    this.domElement.style.width = this.width + "px";
+    this.domElement.style.height = this.height + "px";
+    this.domElement.style.left = this.positionX + "px";
+    this.domElement.style.bottom = this.positionY + "px";
     const boardElm = document.getElementById("board");
     boardElm.appendChild(this.domElement);
   }
   moveRigth() {
-    if (this.positionX >= 95) {
+    if (this.positionX >= 750) {
       this.positionX;
     } else {
       this.positionX = this.positionX + 5;
     }
-    this.domElement.style.left = this.positionX + "vw";
+    this.domElement.style.left = this.positionX + "px";
   }
   moveLeft() {
     if (this.positionX <= 0) {
@@ -32,15 +32,15 @@ class Prisoner {
     } else {
       this.positionX = this.positionX - 5;
     }
-    this.domElement.style.left = this.positionX + "vw";
+    this.domElement.style.left = this.positionX + "px";
   }
   moveUp() {
-    if (this.positionY >= 80) {
+    if (this.positionY >= 450) {
       this.positionY;
     } else {
       this.positionY = this.positionY + 5;
     }
-    this.domElement.style.bottom = this.positionY + "vh";
+    this.domElement.style.bottom = this.positionY + "px";
   }
   moveDown() {
     if (this.positionY <= 0) {
@@ -48,16 +48,36 @@ class Prisoner {
     } else {
       this.positionY = this.positionY - 5;
     }
-    this.domElement.style.bottom = this.positionY + "vh";
+    this.domElement.style.bottom = this.positionY + "px";
   }
 }
+class Hole {
+  constructor() {
+    this.positionX = 750;
+    this.positionY = Math.floor(Math.random() * 450);
+    this.width = 25;
+    this.height = 25;
 
+    this.domElement = null;
+    this.createDomElement();
+  }
+  createDomElement() {
+    this.domElement = document.createElement("div");
+    this.domElement.id = "hole";
+    this.domElement.style.width = this.width + "px";
+    this.domElement.style.height = this.height + "px";
+    this.domElement.style.left = this.positionX + "px";
+    this.domElement.style.bottom = this.positionY + "px";
+    const boardElm = document.getElementById("board");
+    boardElm.appendChild(this.domElement);
+  }
+}
 class TopLight {
   constructor() {
-    this.positionX = Math.floor(Math.random() * 100);
-    this.positionY = 80;
-    this.width = 5;
-    this.height = 10;
+    this.positionX = Math.floor(Math.random() * 750);
+    this.positionY = 450;
+    this.width = 50;
+    this.height = 50;
 
     this.domElement = null;
     this.createDomElement();
@@ -65,29 +85,24 @@ class TopLight {
   createDomElement() {
     this.domElement = document.createElement("div");
     this.domElement.className = "light";
-    this.domElement.style.width = this.width + "vw";
-    this.domElement.style.height = this.height + "vh";
-    this.domElement.style.left = this.positionX + "vw";
-    this.domElement.style.bottom = this.positionY + "vh";
+    this.domElement.style.width = this.width + "px";
+    this.domElement.style.height = this.height + "px";
+    this.domElement.style.left = this.positionX + "px";
+    this.domElement.style.bottom = this.positionY + "px";
     const boardElm = document.getElementById("board");
     boardElm.appendChild(this.domElement);
   }
   moveDown() {
-    this.positionY--;
-    this.domElement.style.bottom = this.positionY + "vh";
-  }
-  removeTopLightifOutside(topLightInstance) {
-    if (topLightInstance.positionY === 0 - topLightInstance.height) {
-      //console.log('remove obstacle with position...', obstacleInstance.positionY);
-      topLightInstance.domElement.remove();
-
-      this.obstacles.shift();
-      console.log(this.obstacles.length);
-    }
+    this.positionY = this.positionY - 5;
+    this.domElement.style.bottom = this.positionY + "px";
   }
 }
+
 const prisoner = new Prisoner();
+const hole = new Hole()
 const topLigths = [];
+const dogs = [];
+
 
 
 document.addEventListener("keydown", (e) => {
@@ -109,7 +124,7 @@ setInterval(() => {
   const newTopLight = new TopLight();
   topLigths.push(newTopLight);
   
-}, 500);
+}, 2000);
 
 //move light & detect collision
 setInterval(() => {
@@ -125,11 +140,38 @@ setInterval(() => {
     prisoner.height + prisoner.positionY > topLightInstance.positionY
   ) {
     console.log("collision");
-    //location.href = 'gameover.html';
+   location.href = 'gameover.html';
   }
-
+  if (topLightInstance.positionY === 0) {
+    //console.log('remove obstacle with position...', obstacleInstance.positionY);
+    topLightInstance.domElement.remove();
+    topLigths.shift();
+    //console.log(this.obstacles.length);
+  }
   });
 
+  
+
 }, 50); 
+
+
+
+setInterval(() => {
+
+  if (
+    prisoner.positionX < hole.positionX + hole.width &&
+    prisoner.positionX + prisoner.width > hole.positionX &&
+    prisoner.positionY < hole.positionY + hole.height &&
+    prisoner.height + prisoner.positionY > hole.positionY
+  ) {
+    console.log("you escaped");
+   location.href = 'win.html';
+  }
+
+  }, 50);
+
+  
+
+
 
 
